5986ce7106fc0f154b913354b300d15f6e9ddbb9
callmeaf <alikhoshkar2000@gmail.com>
2025-04-27 17:12:01 +0330

    add accepted terms column for users

--- src/App/Http/Requests/Api/V1/AuthProfileUpdateRequest.php
+++ src/App/Http/Requests/Api/V1/AuthProfileUpdateRequest.php
@@ -2,8 +2,10 @@
 
 namespace Callmeaf\Auth\App\Http\Requests\Api\V1;
 
+use Callmeaf\Auth\App\Repo\Contracts\AuthRepoInterface;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
 
 class AuthProfileUpdateRequest extends FormRequest
 {
@@ -20,12 +22,12 @@ class AuthProfileUpdateRequest extends FormRequest
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
-    public function rules(): array
+    public function rules(AuthRepoInterface $authRepo): array
     {
         return [
             'first_name' => ['required','string','max:255'],
             'last_name' => ['required','string','max:255'],
-            'mobile' => ['required','digits:11','starts_with:09']
+            'mobile' => ['required','digits:11','starts_with:09',Rule::unique($authRepo->getTable(),'mobile')->ignore($this->user()->id)]
         ];
     }
 }
--- src/App/Http/Resources/Admin/V1/AuthResource.php
+++ src/App/Http/Resources/Admin/V1/AuthResource.php
@@ -24,6 +24,7 @@ class AuthResource extends JsonResource
             'last_name' => $this->last_name,
             'email' => $this->email,
             'mobile' => $this->mobile,
+            'accepted_terms' => $this->accepted_terms,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
             'deleted_at' => $this->deleted_at,
